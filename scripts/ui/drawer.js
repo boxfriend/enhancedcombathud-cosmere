3f5358b811579f2e7e7192b9886be8d8755aca39
boxfriend <cawills394@gmail.com>
2025-05-19 18:08:37 -0400

    now uses built in data and localization

--- scripts/ui/drawer.js
+++ scripts/ui/drawer.js
@@ -1,62 +1,32 @@
 const ARGON = CONFIG.ARGON;
 
-const SKILLS = {
-    "abr": "Abrasion",
-    "adh": "Adhesion",
-    "agi": "Agility",
-    "ath": "Athletics",
-    "chs": "Cohesion",
-    "cra": "Crafting",
-    "dec": "Deception",
-    "ded": "Deduction",
-    "dis": "Discipline",
-    "dvs": "Division",
-    "grv": "Gravitation",
-    "hwp": "Heavy Weapons",
-    "ill": "Illumination",
-    "inm": "Intimidation",
-    "ins": "Insight",
-    "lea": "Leadership",
-    "lor": "Lore",
-    "lwp": "Light Weapons",
-    "med": "Medicine",
-    "prc": "Perception",
-    "prg": "Progression",
-    "prs": "Persuasion",
-    "stl": "Stealth",
-    "sur": "Survival",
-    "thv": "Thievery",
-    "trp": "Transportation",
-    "trs": "Transformation",
-    "tsn": "Tension"
-};
 export default class CosmereSkillsDrawer extends ARGON.DRAWER.DrawerPanel {
     constructor(...args) {
         super(...args);
     }
 
-    get title() { return "Skills"; }
+    get title() { return game.i18n.localize("COSMERE.Actor.Skill.label_p"); }
 
     get categories() {
+        const skills = CONFIG.COSMERE.skills;
 
         const skillButtons = [];
-        const skills = this.actor.system.skills;
+        const actorSkills = this.actor.system.skills;
         for (const skill in skills) {
-            if (skills[skill].unlocked === false) { continue; }
+            if (actorSkills[skill]?.unlocked === false) { continue; }
 
             skillButtons.push(new ARGON.DRAWER.DrawerButton([{
-                label: SKILLS[skill],
+                label: game.i18n.localize(skills[skill].label),
                 onClick: () => {
                     this.actor.rollSkill(skill);
                 }
             }]));
         }
-        console.log(skillButtons);
-        console.log(skills);
+
         return [{
             captions: [
                 {
-                    label: "Skills",
+                    label: game.i18n.localize("COSMERE.Actor.Skill.label"),
                     align: "center",
                 }
             ],
