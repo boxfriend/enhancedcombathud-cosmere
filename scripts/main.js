e285674e03127710b445d2dd5a946097d1296a57
boxfriend <cawills394@gmail.com>
2025-05-19 17:15:51 -0400

    empty weapon sets to get rid of error

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,11 @@
 console.log('hello world');
 import CosmerePortrait from "./ui/portrait.js";
+import CosmereWeaponSets from "./ui/weapon-sets.js"
 Hooks.once("argonInit", (CoreHUD) => {
     console.log("Loading Cosmere Combat HUD");
     CoreHUD.definePortraitPanel(CosmerePortrait);
     CoreHUD.defineMainPanels([CoreHUD.ARGON.PREFAB.PassTurnPanel]);
+
+    CoreHUD.defineWeaponSets(CosmereWeaponSets);
     CoreHUD.defineSupportedActorTypes(["character", "adversary"]);
 });
---
+++ scripts/ui/weapon-sets.js
@@ -0,0 +1,4 @@
+export default class CosmereWeaponSets extends CONFIG.ARGON.WeaponSets {
+    async getDefaultSets() { return {}; }
+    async _onSetChange() { }
+}
