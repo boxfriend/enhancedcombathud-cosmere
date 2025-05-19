40413b2db45bf3738874a9037cb48f5c5f6a149a
boxfriend <cawills394@gmail.com>
2025-05-19 17:14:38 -0400

    added portrait info

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,8 @@
 console.log('hello world');
+import CosmerePortrait from "./ui/portrait.js";
+Hooks.once("argonInit", (CoreHUD) => {
+    console.log("Loading Cosmere Combat HUD");
+    CoreHUD.definePortraitPanel(CosmerePortrait);
+    CoreHUD.defineMainPanels([CoreHUD.ARGON.PREFAB.PassTurnPanel]);
+    CoreHUD.defineSupportedActorTypes(["character", "adversary"]);
+});
---
+++ scripts/ui/portrait.js
@@ -0,0 +1,33 @@
+const ARGON = CONFIG.ARGON;
+
+export default class CosmerePortrait extends ARGON.PORTRAIT.PortraitPanel {
+
+    get description() {
+        return this.actor.name;
+    }
+
+    async getStatBlocks() {
+        const system = this.actor.system;
+        return [
+            [
+                { text: "HP:" },
+                { text: system.resources.hea.value },
+                { text: "/" },
+                { text: system.resources.hea.max.value }
+            ],
+            [
+                { text: "Foc:" },
+                { text: system.resources.foc.value },
+                { text: "/" },
+                { text: system.resources.foc.max.value }
+            ],
+            [
+                { text: "Inv:" },
+                { text: system.resources.inv.value },
+                { text: "/" },
+                { text: system.resources.inv.max.value }
+            ]
+        ];
+
+    }
+}
