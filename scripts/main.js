e6674c67dbbeed553b7f5ba4cebf1cd90c98594c
boxfriend <cawills394@gmail.com>
2025-05-20 14:29:20 -0400

    weapon actions from active weapon sets

--- scripts/main.js
+++ scripts/main.js
@@ -3,12 +3,13 @@ import CosmereSkillsDrawer from "./ui/drawer.js";
 import CosmereWeaponSets from "./ui/weapon-sets.js"
 import CosmereMovementHUD from "./ui/movement-hud.js";
 import CosmereRestButtons from "./ui/rest-buttons.js";
+import CosmereActionHUD from "./ui/action-hud.js";
 
 Hooks.once("argonInit", (CoreHUD) => {
     console.log("Loading Cosmere Combat HUD");
     CoreHUD.definePortraitPanel(CosmerePortrait);
     CoreHUD.defineDrawerPanel(CosmereSkillsDrawer);
-    CoreHUD.defineMainPanels([CoreHUD.ARGON.PREFAB.PassTurnPanel]);
+    CoreHUD.defineMainPanels([CosmereActionHUD, CoreHUD.ARGON.PREFAB.PassTurnPanel]);
 
     CoreHUD.defineWeaponSets(CosmereWeaponSets);
     CoreHUD.defineMovementHud(CosmereMovementHUD);
---
+++ scripts/ui/action-hud.js
@@ -0,0 +1,45 @@
+class CosmereWeaponButton extends CONFIG.ARGON.MAIN.BUTTONS.ItemButton {
+
+    get label() {
+        let name = this.item.name;
+        if (this.item.system.equip.hand === 'off_hand') {
+            name += " " + game.i18n.localize("COSMERE.Item.Equip.Hand.Off.Label");
+        }
+        return name;
+    }
+    async _onLeftClick(event) {
+        this.actor.useItem(this.item);
+    }
+}
+
+export default class CosmereAttackHUD extends CONFIG.ARGON.MAIN.ActionPanel {
+    get label() {
+        return game.i18n.localize("COSMERE.Item.Weapon.Strike");
+    }
+
+    async _getButtons() {
+        const buttons = [
+            new CosmereWeaponButton({
+                isWeaponSet: true,
+                inActionPanel: true,
+                isPrimary: true,
+            }),
+            new CosmereWeaponButton({
+                isWeaponSet: true,
+                inActionPanel: true,
+                isPrimary: false,
+            })
+        ];
+
+        const unarmed = this.actor.items.find((item) => item.name === 'Unarmed Strike');
+        buttons.push(new CosmereWeaponButton({
+            item: unarmed,
+            inActionPanel: true,
+        }));
+
+        console.log("buttons", buttons);
+        return buttons;
+    }
+
+}
+
