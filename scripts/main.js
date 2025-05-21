b9ce62ac01f612eea3869fae330300feb7d1a43e
boxfriend <cawills394@gmail.com>
2025-05-21 12:14:42 -0400

    implement action hud with split buttons to accommodate all actions

--- scripts/main.js
+++ scripts/main.js
@@ -4,12 +4,17 @@ import CosmereWeaponSets from "./ui/weapon-sets.js"
 import CosmereMovementHUD from "./ui/movement-hud.js";
 import CosmereRestButtons from "./ui/rest-buttons.js";
 import CosmereStrikeHUD from "./ui/strike-hud.js";
+import CosmereActionHUD from "./ui/action-hud.js";
 
 Hooks.once("argonInit", (CoreHUD) => {
     console.log("Loading Cosmere Combat HUD");
     CoreHUD.definePortraitPanel(CosmerePortrait);
     CoreHUD.defineDrawerPanel(CosmereSkillsDrawer);
     CoreHUD.defineMainPanels([CosmereStrikeHUD, CoreHUD.ARGON.PREFAB.PassTurnPanel]);
+    CoreHUD.defineMainPanels([
+        CosmereStrikeHUD,
+        CosmereActionHUD,
+        CoreHUD.ARGON.PREFAB.PassTurnPanel]);
 
     CoreHUD.defineWeaponSets(CosmereWeaponSets);
     CoreHUD.defineMovementHud(CosmereMovementHUD);
---
+++ scripts/ui/action-hud.js
@@ -0,0 +1,79 @@
+const BUTTONS = CONFIG.ARGON.MAIN.BUTTONS;
+
+class CosmereItemButton extends BUTTONS.ItemButton {
+
+    async _onLeftClick(event) {
+        this.actor.useItem(this.item);
+    }
+}
+
+export default class CosmereActionHUD extends CONFIG.ARGON.MAIN.ActionPanel {
+    get actionCost() { return 1; }
+    get actionType() { return 'act'; }
+
+    get label() {
+        switch(this.actionType) {
+            case 'act':
+                return this._getActionLabel();
+            case 'fre':
+                return "▷";
+            case 'rea':
+                return "↩";
+            default:
+                return "UNKNOWN";
+        }
+    }
+
+    _getActionLabel() {
+        let label = "";
+        for(let i = 0; i < this.actionCost; i++)
+            label += "▶";
+
+        return label;
+    }
+    async _getButtons() {
+        let actions = this.actor.items?.filter(
+            item => {
+                const system = item.system;
+                return system.activation?.cost?.type === this.actionType
+                    && item.type !== 'weapon' && item.name !== 'Strike';
+            });
+
+        if(this.actionType == 'act')
+            actions = actions.filter(
+                item => item.system.activation?.cost?.value === this.actionCost);
+
+        if(actions && actions.length === 1)
+            return [new CosmereItemButton({
+                item: actions[0],
+                inActionPanel: true,
+            })];
+
+        if(actions && actions.length % 2 !== 0)
+            actions.push(null);
+
+        const buttons = [];
+
+        actions.forEach(item => {
+            if(item)
+                buttons.push(new CosmereItemButton({
+                    item: item,
+                    inActionPanel: true,
+                }));
+            else
+                buttons.push(new BUTTONS.ActionButton());
+        });
+
+        const splitButtons = [];
+        for(let i = 0; i < buttons.length; i += 2) {
+            const first = buttons[i];
+            const second = buttons[i + 1];
+            splitButtons.push(new BUTTONS.SplitButton(first, second));
+        }
+
+        return splitButtons;
+    }
+
+    get template() { return new CONFIG.ARGON.MAIN.ActionPanel().template; }
+}
+
