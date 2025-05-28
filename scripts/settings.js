baa4efe7054bd13664bf73585d961881035a1fe1
boxfriend <cawills394@gmail.com>
2025-05-28 15:16:42 -0400

    setting for including imported/world actions

--- scripts/settings.js
+++ scripts/settings.js
@@ -2,8 +2,18 @@ import { MODULE_ID } from './utilities.js';
 
 export default function registerSettings() {
     game.settings.register(MODULE_ID, "includeBasicActions", {
-        name: "Include Basics Actions",
+        name: "Include Compendium Actions",
         hint: "Optionally include basic actions from the compendium in the action hud",
+        scope: "client",
+        config: true,
+        default: true,
+        type: Boolean,
+        onChange: () => ui.ARGON.refresh(),
+    });
+
+    game.settings.register(MODULE_ID, "includeWorldBasicActions", {
+        name: "Include World Actions",
+        hint: "Optionally include basic actions that have been imported into the world in the action hud",
         scope: "client",
         config: true,
         default: true,
