4c68a141a61ffd696c1b52ae60e3a4db3eba2159
boxfriend <cawills394@gmail.com>
2025-06-10 00:01:46 -0400

    display available compendiums TODO: handle saving selected and only using those ones

---
+++ scripts/ui/compendium-selection-form.js
@@ -0,0 +1,54 @@
+import { MODULE_ID, ITEM_COMPENDIUMS } from "../utilities.js";
+const { ApplicationV2, HandlebarsApplicationMixin } = foundry.applications.api;
+
+export default class CompendiumSelectionForm extends HandlebarsApplicationMixin(ApplicationV2) {
+    static DEFAULT_OPTIONS = {
+        ...super.DEFAULT_OPTIONS,
+        tag: "form",
+        form: {
+            handler: this.handleForm,
+            submitOnChange: false,
+            closeOnSubmit: true,
+        },
+        window: {
+            title: "Select Included Compendiums",
+            contentClasses: ["standard-form"]
+        }
+    };
+
+    static PARTS = {
+        form: {
+            template: `modules/${MODULE_ID}/templates/compendium-selection.hbs`,
+        },
+        footer: {
+            template: "templates/generic/form-footer.hbs",
+        }
+    };
+
+    async _prepareContext(options) {
+        const compendiums = [];
+        const saved = game.settings.get(MODULE_ID, "selectedCompendiums") || [];
+        console.log("rendering compendium selection...");
+        for(const id of ITEM_COMPENDIUMS) {
+            const compendium = game.packs.get(id);
+            compendiums.push({
+                name: compendium.metadata.label,
+                id,
+                checked: saved.includes(id)
+            });
+        }
+        return {
+            compendiums,
+            buttons: [
+                { type: "submit", icon: "fa-solid fa-save", label: "Save Selected" },
+                { type: "button", icon: "fa-solid fa-arrows-rotate", label: "Refresh List" },
+                { type: "cancel", icon: "fa-solid fa-cancel", label: "Cancel" },
+            ]
+        }
+    }
+
+    static async handleForm(event, form, data) {
+
+
+    }
+}
