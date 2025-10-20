6b682ffe4f98c02eebe840da6d35808069ee7c6b
boxfriend <cawills394@gmail.com>
2025-10-20 12:58:32 -0400

    properly updating items now

--- scripts/ui/weapon-sets.js
+++ scripts/ui/weapon-sets.js
@@ -1,18 +1,17 @@
 export default class CosmereWeaponSets extends CONFIG.ARGON.WeaponSets {
     async _onSetChange({sets, active}) {
+        const now = Date.now();
 
         for(const id in sets) {
             const set = sets[id];
 
             const primary = set["primary"]?.system;
-            primary && (primary.equipped = false);
-
             const secondary = set["secondary"]?.system;
-            secondary && (secondary.equipped = false);
 
             const secondaryTwoHand = secondary && secondary.equip.hold === 'two_handed';
             const twoHand = secondaryTwoHand ||
                 (secondary && primary && primary.equip.hold === 'two_handed');
+
             if(twoHand || (secondary && primary === secondary)) {
                 await this._onContextMenu({
                     preventDefault: () => {},
@@ -26,20 +25,24 @@ export default class CosmereWeaponSets extends CONFIG.ARGON.WeaponSets {
                 });
                 return;
             }
-        }
 
-        const activeSet = sets[active];
-
-        const primary = activeSet["primary"]?.system;
-        if(primary) {
-            primary.equipped = true;
-            primary.equip.hand = 'main_hand';
-        }
-
-        const secondary = activeSet["secondary"]?.system;
-        if(secondary && secondary.equip.hold !== 'two_handed'){
-            secondary.equipped = true;
-            secondary.equip.hand = 'off_hand';
+            for(const hand in set)
+            {
+                const weapon = set[hand];
+                const weaponSystem = weapon?.system;
+                if (weaponSystem) {
+                    weaponSystem.equipped = isActive;
+                    weaponSystem.equip.hand = hand === 'primary' ? 'main_hand' : 'off_hand';
+                    const update = {
+                        system: {
+                            equipped: isActive,
+                            equip: { hand: weaponSystem.equip.hand }
+                        },
+                        _stats: { modifiedTime: now }
+                    }
+                    await weapon.update(update);
+                }
+            }
         }
     }
 }
